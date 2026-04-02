da1ecdcbf321891960170653f7a335d67e1f3abd
OskarGorgis <oskar.gorgis@poczta.onet.pl>
2026-04-02 23:18:57 +0200

    Added pipeline to generate samples from midi llm

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import data.data_loader as data_loader
 from data.midisample_class import MidiSample
 from config import XMIDI_DATA_DIR
+from model.midillm.pipeline import generate_samples
 
 if __name__ == "__main__":
-    data_loader.download_and_extract()
-    midi_samples: list[MidiSample] = data_loader.load_data(XMIDI_DATA_DIR)
-    print(f"Loaded {len(midi_samples)} MIDI samples.")
+    # data_loader.download_and_extract()
+    # midi_samples: list[MidiSample] = data_loader.load_data(XMIDI_DATA_DIR)
+    # print(f"Loaded {len(midi_samples)} MIDI samples.")
+    generate_samples()
